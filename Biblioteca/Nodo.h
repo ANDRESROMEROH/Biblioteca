fb9ec22b816ed881b0495779c98cfda71a7d5e6a
Andrés Gutiérrez <andres.gutierrez.arcia@gmail.com>
2017-11-04 14:35:28 -0600

    Cambio de algoritmos
    https://rosettacode.org/wiki/AVL_tree

--- Biblioteca/Nodo.h
+++ Biblioteca/Nodo.h
@@ -1,18 +1,19 @@
 #pragma once
 using namespace std;
 
-template<class T>
+template <class T>
 class Nodo {
 public:
-	Nodo();
-	Nodo(T*, Nodo<T>*, Nodo<T>*); // Dato, hijo izquierdo, hijo derecho.
-	T* getContenido();
-	
-	Nodo<T>* izquierdo;
+	Nodo(T*,Nodo<T>*,Nodo<T>*,Nodo<T>*);
+	Nodo<T>* izquierda;
 	Nodo<T>* derecho;
-
+	Nodo<T>* padre;
+	T* getContenido();
+	int getFactorEquilibrio();
+	void setFactorEquilibrio(int);
 private:
 	T* contenido;
+	int factorEquilibrio;
 };
 
 /////////////////////////| IMPLEMENTACION |//////////////////////////
@@ -24,11 +25,15 @@ ostream& operator<<(ostream& os, Nodo<T>& t) {
 }
 
 template<class T>
-Nodo<T>::Nodo() {}
-
-template<class T>
-Nodo<T>::Nodo(T* _contenido, Nodo<T>* iz, Nodo<T>* der):contenido(_contenido),
-izquierdo(iz),derecho(der){}
+Nodo<T>::Nodo(T* _contenido, Nodo<T>* _papa, Nodo<T>* _izquierda, Nodo<T>* _derecho):
+	contenido(_contenido),izquierda(_izquierda),
+	derecho(_derecho),padre(_papa),factorEquilibrio(0){}
 
 template<class T>
 T* Nodo<T>::getContenido() { return contenido; }
+
+template<class T>
+int Nodo<T>::getFactorEquilibrio() { return this->factorEquilibrio; }
+
+template<class T>
+void Nodo<T>::setFactorEquilibrio(int n) { this->factorEquilibrio = n; }
